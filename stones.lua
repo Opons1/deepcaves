7f569a667ca28b08b77a2543a62ee8c0ccbb51b3
Opons1 <tazimc1001@gmail.com>
2026-07-09 16:46:43 -0700

    madness

--- stones.lua
+++ stones.lua
@@ -1,30 +1,81 @@
 deepcaves.stones = {}
 local function register_stone(texture, name, description, level, tier, max_digs)
+    local variants
+    if max_digs <= 6 then
+           variants = max_digs
+    else
+        variants = 6
+    end
+
+    local overlays = {}
+    local crackstep = variants / 5
+    for i = 0, variants do
+        local crack = math.round(crackstep * i)
+        if i > 0 then
+            table.insert(overlays, "^[crack:1:" .. i)
+        else
+            table.insert(overlays, "")
+        end
+    end
+
+    local nodes = {}
+
+    local nodes = {}
+
+    if variants <= 6 then
+        for i = 1, variants - 1 do
+            nodes[i] = "deepcaves:" .. name .. (i + 1)
+        end
+    else
+        local function findstep(i)
+            return math.max(math.ceil(6 * i / max_digs), 6)
+        end
+        
+        local prev_successful_index = 1
+        for i = 1, max_digs do
+            local step = findstep(i)
+            
+            if step ~= prev_successful_index then
+                nodes[i] = "deepcaves:" .. name .. step
+                prev_successful_index = step
+            end
+        end
+    end
+
     
-    core.register_node("deepcaves:" .. name, {
-        title = title,
-        groups = {cracky = level},
-        tiles = {texture .. "^[crack:1:2"},
-        node_dig_prediction = "deepcaves:" .. name,
-        on_dig = function(pos, node, digger)
-            local current_digs = node.param2
+    for i = 1, variants do
+        local fullname = "deepcaves:" .. name
+        if i ~= 1 then
+            fullname = fullname .. i
+        end
 
-            if current_digs >= max_digs then
-                core.node_dig(pos, node, digger)
-                return
-            end
+        local crack_overlay = overlays[i]
+        core.register_node(fullname, {
+            title = title,
+            groups = {cracky = level},
+            tiles = {texture .. overlays[i]},
+            node_dig_prediction = fullname,
+            on_dig = function(pos, node, digger)
+                local current_digs = node.param2
 
-            node.param2 = current_digs + 1
-            core.swap_node(pos, node)
+                if current_digs >= max_digs - 1 then
+                    core.node_dig(pos, node, digger)
+                    return
+                end
+                if nodes[i] then node.name = nodes[i] end
+                node.param2 = current_digs + 1
 
-            local inventory = digger:get_inventory()
-            if inventory then
-                inventory:add_item("main", "default:cobble 1")
-            end
+                core.swap_node(pos, node)
 
-            core.sound_play("default_cool_lava", {pos = pos, gain = 0.5})
-        end,
-    })
+                local inventory = digger:get_inventory()
+                if inventory then
+                    inventory:add_item("main", "default:cobble 1")
+                end
+
+             core.sound_play("default_cool_lava", {pos = pos, gain = 0.5})
+            end,
+        })
+    end
     table.insert(deepcaves.stones, {
         texture = texture,
         name = name,
